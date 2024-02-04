084ba171746678552d06980e40a74476c553908c
Udbhav Srivastava <udbhavsrivastava.27@gmail.com>
2024-02-04 14:45:40 +0530

    fixing skills icon to be large

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -12,13 +12,14 @@ const Skills = () => {
         <h1 className='text-[40px] font-bold'>{AppText.Skills}<span className='text-purple-600'>{AppText.Experties}</span></h1>
         </div>
         <div className='flex flex-col md:flex-row  justify-evenly px-8 md:px-0  '>
-          <div className='grid grid-cols-2 md:grid-cols-5 gap-10  items-center'>
+          <div className='grid grid-cols-2 md:grid-cols-5
+          gap-10  items-center'>
             {skillsList.map((item, index)=>(
               < div className='flex flex-col items-center'>
-                <div className='w-[60px] md:w-[100px] group transition ease-in-out delay-100 group hover:scale-125 bg-purple-50
+                <div className='w-[100px] group transition ease-in-out delay-100 group hover:scale-125 bg-purple-50
                  p-5 rounded-full' >
                     <img src={item.icon} 
-                    className="w-[80px] hover:animate-spin" alt='icons img'/>
+                    className="w-[90px] hover:animate-spin " alt='icons img'/>
                     
                 </div>
                 <h1 className=' p-4 group hover:font-semibold text-lg'>{item.name}</h1>
